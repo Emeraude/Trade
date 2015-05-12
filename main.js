b6556d095a2be14d213edc067cea3ecf96a8ba29
duques_g <gregory.duquesnoy@epitech.eu>
2015-05-12 22:53:58 +0200

    setting realine
    	now working with moulinette..

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
  
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+// process.stdin.resume();
+// process.stdin.setEncoding('utf8');
 var capital;
 var days;
 
@@ -20,7 +25,7 @@ function sell(n) {
 }
 
 function buy(n) {
-    // process.stdout.write('buy ' + n);
+     // process.stdout.write('buy ' + n);
     console.log('buy ' + n);
 }
 
@@ -32,24 +37,26 @@ function main() {
     var i = 0;
     var values = [];
 
-    process.stdin.on('data', function (chunk) {
+    rl.on('line', function (chunk) {
 
-	if (chunk == '--end--\n')
-	    process.exit(0);
-	if (i < 2) {
-	    getArg(chunk, i);
-	}
-	else {
-	    console.log("index: " + i);
-	    values.push(chunk);
-	    if (i > 2 && values[values.length - 1] < values[values.length - 2])
-	    	sell(2);
-	    else
-	    	buy(5);
-	fs.writeFile("tmp.txt", chunk);
-	}
-	i = i + 1;
+    	    console.error("index: " + i);
+    	if (chunk == '--end--\n')
+    	    process.exit(0);
+    	if (i < 2) {
+    	    getArg(chunk, i);
+    	}
+    	else {
+    	    values.push(chunk);
+    	    if (i > 2 && values[values.length - 1] > values[values.length - 2])
+    	    	buy(1);
+    	    else
+    	    	wait();
+    	}
+    	fs.writeFile("tmp.txt", chunk);
+    	i = i + 1;
     });
+    
+    
 }
 
 main();
