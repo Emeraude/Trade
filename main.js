86ded34907c2c3fceb2cdf6ec451ccbdd08bb79d
duques_g <gregory.duquesnoy@epitech.eu>
2015-05-14 02:52:22 +0200

    program working correctly with moulinette
    	stiil has to implement correct IA

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ var rl = readline.createInterface({
 // process.stdin.setEncoding('utf8');
 var capital;
 var days;
+    var actions = 0;
 
 function getArg(chunk, i) {
     if (i == 0)
@@ -21,12 +22,13 @@ function getArg(chunk, i) {
 
 function sell(n) {
     console.log('sell ' + n);
-    // process.stdout.write('sell ' + n);
+    actions -= n;
 }
 
 function buy(n) {
      // process.stdout.write('buy ' + n);
     console.log('buy ' + n);
+    actions += n;
 }
 
 function wait() {
@@ -36,28 +38,36 @@ function wait() {
 function main() {
     var i = 0;
     var values = [];
-
     rl.on('line', function (chunk) {
 
-    	    console.error("index: " + i);
-    	if (chunk == '--end--' || chunk == '--END--'
-	    || chunk == '-end-' || chunk == '-END-')
+    	console.error("index: " + i);
+	console.error('days: ' + days)
+	console.error('data: ' + chunk + "\n")
+	    console.error('actions are :' + actions);
+    	if (chunk.match(/\-end\-/i)) {
     	    process.exit(0);
+	}
     	else if (i < 2) {
     	    getArg(chunk, i);
     	}
+	else if (i - 1 == days) {
+	    sell(actions, actions);
+	    console.error('selling everything at: ' + i + 'action: ' + actions);
+	}
+	else if (i > days)
+	    wait()
     	else {
     	    values.push(chunk);
-    	    if (i > 2 && values[values.length - 1] > values[values.length - 2])
-    	    	;
-    	    else
-    	    	wait();
+    	    if (i > 2 && capital > chunk && values[values.length - 1] < values[values.length - 2])
+    	    	buy(3);
+    	    else if (actions > 0)
+    	    	sell(1);
+	    else
+		wait()
     	}
     	fs.writeFile("tmp.txt", chunk);
     	i = i + 1;
     });
-    
-    
 }
 
 main();
