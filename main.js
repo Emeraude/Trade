45cd184d3d39d77947e2d921f9383bfd454ccf9f
duques_g <gregory.duquesnoy@epitech.eu>
2015-05-22 04:56:58 +0200

    IA do benefice on almost every data

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
- 
+
 // process.stdin.resume();
 // process.stdin.setEncoding('utf8');
 var capital;
@@ -14,6 +14,8 @@ var days;
 var actions = 0;
 var values = [];
 var last = 0;
+
+var costAction = 0;
 
 function getArg(chunk, i) {
     if (i == 0)
@@ -30,9 +32,11 @@ function sell(n) {
 
 function buy(n) {
     console.error('capital before is:' + capital + ' n is : ' + n);
+
     console.log('buy ' + n);
     actions += n;
     capital -= Math.ceil((values[last] + values[last] * 0.15 / 100)) * n;
+    costAction = values[last];
     console.error('capital after is:' + capital);
 }
 
@@ -40,10 +44,15 @@ function wait() {
     console.log('wait');
 }
 
+function getTrend() {
+
+}
+
 function getMMA(n) {
     var i = 0;
     var j = values.length - 1;
     var average = 0;
+
     for (i = 0; i < n && j >= 0 ; ++i) {
 	average += values[j];
 	--j;
@@ -52,23 +61,53 @@ function getMMA(n) {
     return (average);
 }
 
+function getEMA(n) {
+    var i = 0;
+    var j = values.length - 1;
+    var average = 0;
+    if (n > values.length - 1)
+	n = values.length - 1;
+    else
+	n = values.length - 1 - n;
+    // MACRO
+    for (i = n; i < values.length - 1; ++i) {
+	average = average * 0.91 + values[i] * 0.09;
+    }
+    return (average);
+}
+
+function getMACD() {
+    return (getEMA(12) - getEMA(26));
+}
+
 function decisionMaker(day) {
+    var mma150 = getMMA(150);
     var mma30 = getMMA(30);
     var mma5 = getMMA(5);
+    var ema12 = getEMA(12);
+    var macd = getMACD();
+
+    console.error('EMA12 is: ' + ema12);
+    console.error('MACD is: ' + macd);
+    if (ema12 < mma5)
+	console.error('inferior: ' + (mma5 - ema12));
+    else
+	console.error('superior');
+
+    console.error('mma150 is: ' + mma150);
     console.error('mma30 is: ' + mma30);
     console.error('mma5 is: ' + mma5);
     console.error('capital is: ' + capital);
-    if (mma5 < mma30)
+    if (macd < 0 && actions > 0)
     	sell(actions);
-    else if (mma5 > mma30)
+    else if (macd > 0 && capital > values[last])
     	buy(Math.floor(capital / Math.ceil((values[last] + (values[last] * 0.15) / 100))));
     else
     	wait();
-    // if (capital > values[values.length - 1]
-    // 	&& values[values.length - 1] > values[values.length - 2])
-    // 	buy(3);
-    // else if (actions > 0)
-    // 	sell(1);
+    // if (mma5 < mma30 && actions > 0)
+    // 	sell(actions);
+    // else if (mma5 > mma30 && capital > values[last])
+    // 	buy(Math.floor(capital / Math.ceil((values[last] + (values[last] * 0.15) / 100))));
     // else
     // 	wait();
 }
