44bb2ed3301c55b63aca91e099cd2c71bf8c55dc
duques_g <gregory.duquesnoy@epitech.eu>
2015-05-22 15:55:18 +0200

    adding stochastique & fixing bugs
           adding integral stochastique
           fixing loops

--- main.js
+++ main.js
@@ -9,10 +9,13 @@ var rl = readline.createInterface({
 
 // process.stdin.resume();
 // process.stdin.setEncoding('utf8');
-var capital;
-var days;
+var capital = 0;
+var days = 0;
 var actions = 0;
+
 var values = [];
+var macdTab = [];
+
 var last = 0;
 
 var costAction = 0;
@@ -44,93 +47,153 @@ function wait() {
     console.log('wait');
 }
 
-function getTrend() {
-
-}
-
-function getMMA(n) {
+function getMMA(data, n) {
     var i = 0;
-    var j = values.length - 1;
+    var j = last + 1;
     var average = 0;
 
-    for (i = 0; i < n && j >= 0 ; ++i) {
-	average += values[j];
+    for (i = 0; i <= n && j >= 0 ; ++i) {
+	average += data[j];
 	--j;
     }
     average /= i;
     return (average);
 }
 
+
+var EMAcoef = 0.10;
 function getEMA(n) {
     var i = 0;
-    var j = values.length - 1;
     var average = 0;
-    if (n > values.length - 1)
-	n = values.length - 1;
+
+    if (n > values.length)
+	n = values.length;
     else
-	n = values.length - 1 - n;
-    // MACRO
-    for (i = n; i < values.length - 1; ++i) {
-	average = average * 0.91 + values[i] * 0.09;
+	n = values.length - n;
+
+    for (i = n; i <= last; ++i) {
+	average = average * (1 - EMAcoef) + values[i] * EMAcoef;
     }
-    return (average);
+    return average;
 }
 
 function getMACD() {
-    return (getEMA(12) - getEMA(26));
+    return getEMA(12) - getEMA(26);
+}
+
+var kTab = [];
+function getStochastique(n2) {
+    var k;
+    var d;
+
+    var i = 0;
+    var highest = values[last]; //highest hight
+    var lowest = values[last]; // lowest low
+
+    console.error("i is :" + i + ", last: " + last + ", n: " + n2);
+    if (n2 > last)
+	n2 = last + 1;
+    else
+	n2 = last - n2 + 1;
+
+    for (i = n2; i < last; ++i) {
+	console.error(values[i]);
+	if (lowest > values[i])
+	    lowest = values[i];
+	if (highest < values[i])
+	    highest = values[i];
+    }
+    console.error('last: '+ values[last]);
+    console.error("");
+    console.error("highest is :" + highest);
+    console.error("lowest is :" + lowest);
+
+    //should not be values which is from open, should be close
+    k = 100 * ( (values[last] - lowest) / (highest - lowest) );
+    kTab.push(k);
+    d = 3 - getMMA(kTab, 12);
+    console.error("k is :" + k);
+    return k;
+}
+
+function getMaxActionToBuy() {
+    return Math.floor(capital / Math.ceil((values[last] + (values[last] * 0.15) / 100)));
 }
 
 function decisionMaker(day) {
-    var mma150 = getMMA(150);
-    var mma30 = getMMA(30);
-    var mma5 = getMMA(5);
+    var mma150 = getMMA(values, 150);
+    var mma30 = getMMA(values, 30);
+    var mma5 = getMMA(values, 5);
     var ema12 = getEMA(12);
     var macd = getMACD();
+    var stoch = getStochastique(14);
 
-    console.error('EMA12 is: ' + ema12);
-    console.error('MACD is: ' + macd);
-    if (ema12 < mma5)
-	console.error('inferior: ' + (mma5 - ema12));
-    else
-	console.error('superior');
+    // console.error('EMA12 is: ' + ema12);
+    // console.error('MACD is: ' + macd);
+    // if (ema12 < mma5)
+    // 	console.error('inferior: ' + (mma5 - ema12));
+    // else
+    // 	console.error('superior');
+    // console.error('mma150 is: ' + mma150);
+    // console.error('mma30 is: ' + mma30);
+    // console.error('mma5 is: ' + mma5);
+    // console.error('capital is: ' + capital);
 
-    console.error('mma150 is: ' + mma150);
-    console.error('mma30 is: ' + mma30);
-    console.error('mma5 is: ' + mma5);
-    console.error('capital is: ' + capital);
-    if (macd < 0 && actions > 0)
+    if (stoch > 80 && actions > 0)
     	sell(actions);
-    else if (macd > 0 && capital > values[last])
-    	buy(Math.floor(capital / Math.ceil((values[last] + (values[last] * 0.15) / 100))));
+    else if (stoch < 20 && capital > values[last])
+    	buy(getMaxActionToBuy());
     else
-    	wait();
+    	wait();   
+
+    // var tmp = getMMA(macdTab, 10);
+    // if (tmp < macd && actions > 0)
+    // 	sell(actions);
+    // else if (macd > 0 || tmp > macd && capital > values[last])
+    // 	buy(getMaxActionToBuy());
+    // else
+    // 	wait();
+
+    //do not work
+    // if (macd < 0 && actions > 0)
+    // 	sell(actions);
+    // else if (macd > 0 && capital > values[last])
+    // 	buy(getMaxActionToBuy());
+    // else
+    // 	wait();
+
     // if (mma5 < mma30 && actions > 0)
     // 	sell(actions);
     // else if (mma5 > mma30 && capital > values[last])
-    // 	buy(Math.floor(capital / Math.ceil((values[last] + (values[last] * 0.15) / 100))));
+    // 	buy(getMaxActionToBuy());
     // else
     // 	wait();
+
 }
 
 function main() {
     var i = 0;
     rl.on('line', function (data) {
 
-    	console.error("\nindex: " + i);
-	console.error('capital is :' + capital);
+    	// console.error("\nindex: " + i + ' || ' + last);
+	// console.error('capital is :' + capital);
 	// console.error('days: ' + days)
+
 	// console.error('data: ' + data + "\n")
  	// console.error('actions are :' + actions);
+
     	if (data.match(/\-end\-/i))
     	    process.exit(0);
+	    // return (0);
     	else if (i < 2)
     	    getArg(data, i);
-	else if (last == days)
+	else if (i - 1 == days)
 	    sell(actions, actions);
 	else if (i > days)
 	    wait();
     	else if (i > 2) {
     	    values.push(parseInt(data, 10));
+	    macdTab.push(getMACD());
 	    decisionMaker(last);
 	    ++last;
 	}
