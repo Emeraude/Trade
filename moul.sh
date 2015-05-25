c2c7c8425da0f32da5a36b8ab6d6de3650e1911f
Thibaut Broggi <broggi_t@epitech.eu>
2015-05-26 00:35:05 +0200

    adding script to launch all tests

---
+++ moul.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+
+[ $# -eq 1 ] && money=$1 || money=100000
+
+for file in `ls examples/*.txt examples/*.TXT`; do
+    echo -e "\033[36m$(basename $file)\033[0m"
+    ./moulinette.php -f $file -p main.js -m $money
+done
